30e9c99466a6dea0be899e1c10bca0fc8b2fd04a
Julian West <west221b@gmail.com>
2020-04-17 19:45:44 -0400

    Display data formatted

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
+from datetime import timedelta
 import csv
 import os
 import re
@@ -12,6 +13,53 @@ sb6183 = {
     "event log": "http://192.168.100.1/RgEventLog.asp",
     "configuration": "http://192.168.100.1/RgConfiguration.asp"
 }
+product_fields = [
+    'Standard Specification Compliant',
+    'Hardware Version',
+    'Software Version',
+    'Cable Modem MAC Address',
+    'Serial Number',
+    'Up Time'
+]
+status_fields = [
+    'Acquire Downstream Channel',
+    'Connectivity State',
+    'Boot State',
+    'Configuration File',
+    'Security',
+    'DOCSIS Network Access Enabled'
+]
+address_fields = [
+    'Serial Number',
+    'HFC MAC Address',
+    'Known CPE MAC Address',
+    '2'
+]
+downstream_fields = [
+    'Channel',
+    'Lock Status',
+    'Modulation',
+    'Channel ID',
+    'Frequency',
+    'Power',
+    'SNR',
+    'Corrected',
+    'Uncorrectables'
+]
+upstream_fields = [
+    'Channel',
+    'Lock Status',
+    'US Channel Type',
+    'Channel ID',
+    'Symbol Rate',
+    'Frequency',
+    'Power'
+]
+event_fields = [
+    'Time',
+    'Priority',
+    'Description'
+]
 
 
 def get_soup(url: str, page_name: str) -> BeautifulSoup:
@@ -28,15 +76,7 @@ def get_product_information() -> None:
     soup = get_soup(sb6183[key], key)
     soup_tds = soup.find_all('td')
     tds = [soup_td.text for soup_td in soup_tds]
-    fields = [
-        'Standard Specification Compliant',
-        'Hardware Version',
-        'Software Version',
-        'Cable Modem MAC Address',
-        'Serial Number',
-        'Up Time'
-    ]
-    for field in fields:
+    for field in product_fields:
         try:
             value_index = tds.index(field)
         except ValueError:
@@ -50,20 +90,14 @@ def get_addresses() -> None:
     soup = get_soup(sb6183[key], key)
     soup_tds = soup.find_all('td')
     tds = [soup_td.text for soup_td in soup_tds]
-    fields = [
-        'Serial Number',
-        'HFC MAC Address',
-        'Known CPE MAC Address',
-        '2'
-    ]
-    for field in fields:
+    for field in address_fields:
         try:
             value_index = tds.index(field)
         except ValueError:
             print(f"Error: {field} not found in {tds}!")
             continue
         data_frame[field] = tds[value_index + 1]
-    data_frame[fields[-2]] = data_frame.pop(fields[-1])
+    data_frame[address_fields[-2]] = data_frame.pop(address_fields[-1])
 
 
 def get_status() -> None:
@@ -71,15 +105,7 @@ def get_status() -> None:
     soup = get_soup(sb6183[key], key)
     soup_tds = soup.find_all('td')
     tds = [soup_td.text for soup_td in soup_tds]
-    fields = [
-        'Acquire Downstream Channel',
-        'Connectivity State',
-        'Boot State',
-        'Configuration File',
-        'Security',
-        'DOCSIS Network Access Enabled'
-    ]
-    for field in fields:
+    for field in status_fields:
         try:
             value_index = tds.index(field + '\r')
         except ValueError:
@@ -89,26 +115,6 @@ def get_status() -> None:
     downstream_channels = []
     upstream_channels = []
     try:
-        downstream_fields = [
-            'Channel',
-            'Lock Status',
-            'Modulation',
-            'Channel ID',
-            'Frequency',
-            'Power',
-            'SNR',
-            'Corrected',
-            'Uncorrectables'
-        ]
-        upstream_fields = [
-            'Channel',
-            'Lock Status',
-            'US Channel Type',
-            'Channel ID',
-            'Symbol Rate',
-            'Frequency',
-            'Power'
-        ]
         downstream_index = tds.index(downstream_fields[-1]) + 1
         upstream_index = tds.index(upstream_fields[0], downstream_index + 1)
         while downstream_index < upstream_index:
@@ -138,11 +144,6 @@ def get_event_log() -> None:
     tds.pop()  # last td is clear log button
     assert len(tds) % 3 == 0, f"Event log size is {len(tds)} and not divisible by 3!"
     events = []
-    event_fields = [
-        'Time',
-        'Priority',
-        'Description'
-    ]
     td_index = 0
     while td_index < len(tds):
         event = {}
@@ -233,8 +234,37 @@ def display_data() -> None:
         'Information',
         'Debug'
     ]
+    uptime = datetime.now() - timedelta(seconds=data_frame['Up Time'])
+    print(f"\nOnline since {uptime}")
+    print(f"{data_frame['Standard Specification Compliant']}")
+    print(f"{data_frame['Software Version']}")
+    for field in status_fields:
+        print(f"{field:>29}: {data_frame[field][0]} {data_frame[field][1]}")
 
-    [print(f"{key}: {value}") for key, value in data_frame.items()]
+    print("\nDownstream Channels:")
+    for channel in data_frame['Downstream Channels']:
+        print(f"{channel['Channel']:02}:{channel['Channel ID']:02}"
+              f" {channel['Lock Status']:10}"
+              f" {channel['Modulation']:8}"
+              f" {channel['Frequency']:>5} MHz"
+              f" {channel['SNR']:>6} dB"
+              f" {channel['Power']:>5} dBmV"
+              f" {channel['Corrected']:>9}"
+              f" {channel['Uncorrectables']:>9}")
+
+    print("\nUpstream Channels:")
+    for channel in data_frame['Upstream Channels']:
+        print(f" {channel['Channel']}:{channel['Channel ID']} "
+              f" {channel['Lock Status']:10}"
+              f" {channel['US Channel Type']:8}"
+              f" {channel['Frequency']:>5} MHz"
+              f" {channel['Symbol Rate']:>5} K/s"
+              f" {channel['Power']:>5} dBmV")
+
+    print("\nEvent Log:")
+    for event in data_frame['Event Log']:
+        print(f"{event['Time']} {priorities[event['Priority']]:11}"
+              f" {event['Description']}")
 
 
 if __name__ == '__main__':
